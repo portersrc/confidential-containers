f069537787a659b1634eb24ccd90996a1e8daa3b
Chris Porter <porter@ibm.com>
2023-09-26 15:29:07 -0500

    add this source-suffix to config

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,12 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
